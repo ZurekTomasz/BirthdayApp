9f998ef7ca7e2ddeee3d71a33033e4dd773973b6
ZurekTomasz <mail@tomass.net>
2017-07-27 17:29:34 +0200

    Add project files.

---
+++ BirthdayApp/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using BirthdayApp.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BirthdayApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult UsersList()
+        {
+            var userId = User.Identity.GetUserId();
+            var context = new ApplicationDbContext();
+
+            var allUsers = (from i in context.Users
+                           select i).ToList();
+
+            ViewBag.allUsersContext = allUsers;
+
+            return View();
+        }
+    }
+}
---
+++ BirthdayApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BirthdayApp.Startup))]
+namespace BirthdayApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
