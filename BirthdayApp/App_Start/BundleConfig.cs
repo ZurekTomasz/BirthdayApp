75b263eee776834b2fcf69221d42bc612f8a2571
ZurekTomasz <mail@tomass.net>
2017-08-02 15:51:30 +0200

    Add jQuery Datepicker

--- BirthdayApp/App_Start/BundleConfig.cs
+++ BirthdayApp/App_Start/BundleConfig.cs
@@ -26,6 +26,9 @@ namespace BirthdayApp
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/cssjqryUi").Include(
+                      "~/Content/jquery-ui.css"));
         }
     }
 }
