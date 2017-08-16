c26de87fb696c1755a90987ce797b9b5f54070bb
ZurekTomasz <mail@tomass.net>
2017-08-16 11:27:06 +0200

    Add UsersListBox

---
+++ BirthdayApp/Controllers/UsersListController.cs
@@ -0,0 +1,63 @@
+﻿using BirthdayApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BirthdayApp.Controllers
+{
+    public class UsersListController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            UsersListBox fruit = new UsersListBox();
+            fruit.UsersList = PopulateFruits();
+            return View(fruit);
+        }
+
+        [HttpPost]
+        public ActionResult Index(UsersListBox fruit)
+        {
+            fruit.UsersList = PopulateFruits();
+            if (fruit.UsersListIds != null)
+            {
+                List<SelectListItem> selectedItems = fruit.UsersList.Where(p => fruit.UsersListIds.Contains(int.Parse(p.Value))).ToList();
+
+                ViewBag.Message = "Selected Fruits:";
+                foreach (var selectedItem in selectedItems)
+                {
+                    selectedItem.Selected = true;
+                    ViewBag.Message += "\\n" + selectedItem.Text;
+                }
+            }
+
+            return View(fruit);
+        }
+
+        private static List<SelectListItem> PopulateFruits()
+        {
+            List<SelectListItem> items = new List<SelectListItem>();
+            items.Add(new SelectListItem
+            {
+                Text = "Pies",
+                Value = "1"
+            });
+
+            items.Add(new SelectListItem
+            {
+                Text = "Kot",
+                Value = "2"
+            });
+
+            items.Add(new SelectListItem
+            {
+                Text = "Małpa",
+                Value = "3"
+            });
+
+            return items;
+        }
+    }
+}
---
+++ BirthdayApp/Models/UsersListBox.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BirthdayApp.Models
+{
+    public class UsersListBox
+    {
+        public List<SelectListItem> UsersList { get; set; }
+        public int[] UsersListIds { get; set; }
+    }
+}
